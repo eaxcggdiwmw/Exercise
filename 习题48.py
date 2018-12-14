552caadedf50fdeb1efbc47e4cb4b88abb54e14e
huangkai <379864478@qq.com>
2018-12-14 17:08:41 +0800

    modified:   .idea/workspace.xml 	new file:   "\344\271\240\351\242\23048.py" 	new file:   "\344\271\240\351\242\23049_.py" 	new file:   "\344\271\240\351\242\23050_.py" 	new file:   "\344\271\240\351\242\23051_.py" 	new file:   "\344\271\240\351\242\23052_.py" 	new file:   "\344\271\240\351\242\23053_.py" 	new file:   "\344\271\240\351\242\23054.py"

---
+++ 习题48.py
@@ -0,0 +1,11 @@
+# 题目：数字比较。
+# 程序分析：无
+
+x = int(input("pls input x:"))
+y = int(input("pls input y:"))
+if x > y:
+    print(x, ">", y)
+elif x < y:
+    print(x, "<", y)
+else:
+    print(x, "=", y)
