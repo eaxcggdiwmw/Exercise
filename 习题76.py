9b41fc02c3bec899b50c4cce82b0e5d5a6510199
huangkai <379864478@qq.com>
2018-12-25 17:59:20 +0800

    new file:   .idea/dictionaries/DELL.xml 	modified:   .idea/workspace.xml 	modified:   "\344\271\240\351\242\23076.py" 	new file:   "\344\271\240\351\242\23077.py" 	new file:   "\344\271\240\351\242\23078.py" 	new file:   "\344\271\240\351\242\23079.py" 	new file:   "\344\271\240\351\242\23080.py" 	new file:   "\344\271\240\351\242\23081.py"

--- 习题76.py
+++ 习题76.py
@@ -1,3 +1,32 @@
 # 题目：编写一个函数，输入n为偶数时，调用函数求1/2+1/4+...+1/n,当输入n为奇数时，调用函数1/1+1/3+...+1/n
 # 程序分析：无。
 
+
+def func(a):
+    if (a % 2) == 0:
+        func1(a)
+    else:
+        func2(a)
+
+
+def func1(b):
+    j = 0
+    for i in range(1, b):
+        if (b - i) % 2 == 0:
+            j += 1/i
+            print(j)
+    print(j)
+
+
+def func2(c):
+    m = 0
+    for n in range(1, c):
+        if (c - n) % 2 == 0:
+            m += 1/n
+            print(m)
+    print(m)
+
+
+if __name__ == '__main__':
+    num = int(input("pls input num:"))
+    func(num)
---
+++ 习题77.py
@@ -0,0 +1,8 @@
+# 题目：循环输出列表
+# 程序分析：无。
+
+
+a = ['aaa', 'bbb', 'ccc', 'ddd', 'eee']
+
+for i in a:
+    print(i)
